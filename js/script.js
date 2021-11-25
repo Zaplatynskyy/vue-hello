0e684b28bbe6210771581cfc8169bf8d462c1b17
Igor <zaplatynskyy@gmail.com>
2021-11-25 16:29:59 +0100

    consegna

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el : '#root',
+    data : {
+        title : 'Hello Vue!',
+        linkImg : 'https://miro.medium.com/max/855/1*9A5EFag9rsRWjWswxlZ4hA.png' 
+    }
+});
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
